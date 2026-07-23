8694b3fec0a7e65938d14e147fbc51b46bdf4220
MohameAdel <midoadel9618@gmail.com>
2026-07-23 19:51:45 +0300

    style: remove spaces from phone number in footer

--- Downloads/perfectbooth/src/components/layout/Footer.tsx
+++ Downloads/perfectbooth/src/components/layout/Footer.tsx
@@ -76,7 +76,7 @@ export default function Footer() {
               </li>
               <li>
                 <a href="tel:+201116013011" className="footer-link">
-                  <i className="fa-solid fa-phone" style={{ marginInlineEnd: '8px' }}></i> +20 111 601 3011
+                  <i className="fa-solid fa-phone" style={{ marginInlineEnd: '8px' }}></i> +201116013011
                 </a>
               </li>
               <li>
